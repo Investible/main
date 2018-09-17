99b7621847134e14f131d1752db945851920c066
Collier Bluff <collierbluff@Colliers-MacBook-Air.local>
2018-09-17 15:00:13 -0500

    minor pom changes

--- src/main/java/com/codeup/investible/InvestibleApplication.java
+++ src/main/java/com/codeup/investible/InvestibleApplication.java
@@ -3,10 +3,12 @@ package com.codeup.investible;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.*;
+
 @SpringBootApplication
 public class InvestibleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(InvestibleApplication.class, args);
+        run(InvestibleApplication.class, args);
     }
 }
