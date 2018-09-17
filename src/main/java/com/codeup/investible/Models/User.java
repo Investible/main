325da7923fa76b4e5325ed313720047c3be9c0d1
Collier Bluff <collierbluff@Colliers-MacBook-Air.local>
2018-09-17 14:04:20 -0500

    Initial Models

---
+++ src/main/java/com/codeup/investible/Models/User.java
@@ -0,0 +1,31 @@
+package com.codeup.investible.Models;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id @GeneratedValue
+    private long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String password;
+
+    @Column
+    private String email;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Comment> comments;
+}
