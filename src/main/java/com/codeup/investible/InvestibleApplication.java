02ca915ef288064d68d7277c8fb09f475800942d
MRDenny <mrdenny@MRDennys-MacBook-Pro.local>
2018-09-18 09:15:32 -0500

    Consume REST Api Test (Fail)

--- src/main/java/com/codeup/investible/InvestibleApplication.java
+++ src/main/java/com/codeup/investible/InvestibleApplication.java
@@ -1,7 +1,10 @@
 package com.codeup.investible;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 import static org.springframework.boot.SpringApplication.*;
 
@@ -11,4 +14,7 @@ public class InvestibleApplication {
     public static void main(String[] args) {
         run(InvestibleApplication.class, args);
     }
+
+
+
 }
