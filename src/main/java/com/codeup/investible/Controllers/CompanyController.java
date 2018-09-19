bb5d32457011cdb86104d28c3fa14c279582adb6
Collier Bluff <collierbluff@Colliers-MacBook-Air.local>
2018-09-19 16:22:58 -0500

    resolved minor conflict

--- src/main/java/com/codeup/investible/Controllers/CompanyController.java
+++ src/main/java/com/codeup/investible/Controllers/CompanyController.java
@@ -1,27 +1,14 @@
-package com.codeup.investible.Controllers;
+package com.codeup.investible.controllers;
 
-<<<<<<< HEAD:src/main/java/com/codeup/investible/Controllers/CompanyController.java
-import com.codeup.investible.Repository.CompanyRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-=======
-import com.codeup.investible.Models.Company;
-
 import com.codeup.investible.services.CompanyService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
->>>>>>> afa74ccb0215f5ff304ae63a3ada0f44b8c00b4b:src/main/java/com/codeup/investible/controllers/CompanyController.java
+
 
 @Controller
 public class CompanyController {
-    private final CompanyRepository companyRepo;
-
-    public CompanyController(CompanyRepository companyRepo){
-        this.companyRepo = companyRepo;
-    }
 
     private final CompanyService companySvc;
 
